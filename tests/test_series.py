179566576182d5c2c4fa5f837ff3521dfb45c0da
moradalkhatib <muradalkhateb@icloud.com>
2021-10-11 21:43:30 +0300

    add if statment for the function to check the argumant les than zero or equal zero return Incorrect input and test that

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,8 @@
 from math_series.series import fibonacci
 
 def test_firstTest():
-    l=5
-    assert fibonacci(l)==l
+    l=0
+    assert fibonacci(l)=="Incorrect input"
+def test_firstTest1():
+    l=-5
+    assert fibonacci(l)=="Incorrect input"
